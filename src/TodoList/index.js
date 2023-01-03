04fd766409474e4ddba7fcaa45c2ebbd9cc2d288
Daniel Roa <daroap@correo.udistrital.edu.co>
2023-01-02 20:09:26 -0500

    Updating files that I made with the course introduction to react by Platzi

---
+++ src/TodoList/index.js
@@ -0,0 +1,23 @@
+import './TodoList.css'
+import React from 'react';
+
+function TodoList(props) {
+
+    return(
+
+        <section>
+        
+            <ul>
+
+            {props.children}
+
+
+            </ul>
+        
+        </section>
+
+    );
+
+}
+
+export {TodoList};
